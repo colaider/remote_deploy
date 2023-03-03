04984ecdbc378d557cda867ea166e53780675b45
colaider <45285561+colaider@users.noreply.github.com>
2023-03-03 16:01:13 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+const { json, Router } = require('express');
+const express = require('express');
+const app = express();
+const port = 4000;
+var pool = require('./routes/pool')
+
+
+function count(){
+  pool.query('SELECT * FROM Toggle',(error,result) =>{
+      console.log(result[1].date)
+     })
+   }
+
+
+function write(tog,temp, req,res){
+  let date_ob = new Date();
+  let date = ("0" + date_ob.getDate())
+  console.log(date)
+  
+  // pool.query("DELETE FROM Toggle",(err, result)=>{
+
+  // })
+
+  // pool.query(`INSERT INTO Toggle(id,toggle,date,temperature)
+  // VALUES(${1}, ${0},"${date}", ${56})`,(error,result) =>{
+  //          if(error){
+  //              res.send(error)
+  //          }else{
+  //           res.send("hui")
+  //          }
+  // })
+
+  pool.query(`UPDATE Toggle SET id=1, toggle=${tog},  date="${date}", temperature=${temp} WHERE id = 1`,(error,result) =>{
+    if(error){
+      res.send(error);
+    }else{
+      res.send("Ui <<-->>")
+    }
+  })
+}
+
+app.get('/write/:tog/:temp', (req,res) =>{
+  write(req.params.tog, req.params.temp, req, res)
+})
+
+
+app.get('/get', (req, res) => {
+  // res.send('Hello World!');
+  pool.query('SELECT * FROM Toggle',(error,result) =>{
+    if(error){
+        res.send(error);
+    }else{
+        res.send(result)
+    }
+})
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
